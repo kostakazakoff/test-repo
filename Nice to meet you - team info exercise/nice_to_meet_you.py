3c7b2288c82eba282105d2bfbb066fc41ebb1651
Kosta Kazakov <kostakazakoff@gmail.com>
2022-05-25 14:21:23 +0300

    Update nice_to_meet_you.py

--- Nice to meet you - team info exercise/nice_to_meet_you.py
+++ Nice to meet you - team info exercise/nice_to_meet_you.py
@@ -6,17 +6,19 @@ hobby = input('What hobbies/pastimes do they enjoy?: ')
 why = input('Why did you pick SoftUni?: ')
 print('===============================================')
 
-info = name + ' from ' + place + ' with a ' + hobby + ' hobby' + ' pick SoftUni because ' + why + ' :)'
+info = 'I am ' + name + ' from ' + place + '. My hobby is ' + hobby + '. I picked SoftUni because ' + why + ' :)'
 print(info)
 
 ok = False
 while not ok:
     dir_to_save = input('Where you want to save your txt file? (a/path/you/want/to/save/to): ')
-    file_name = input('Enter your file name: ') + '.txt'
+    file_name = input('Enter your file name (your_name): ') + '.txt'
     try:
         f = open(os.path.join(dir_to_save, file_name), "w")
         f.write(info)
         f.close()
         ok = True
     except:
+        print('===============================================================')
         print('Something is wrong! Try again to enter the file name and path: ')
+
