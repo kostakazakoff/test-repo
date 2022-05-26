4618dcdfc59389fe7da7ee1a17e20451924f988f
Kosta Kazakov <kostakazakoff@gmail.com>
2022-05-26 14:09:19 +0300

    Update nice_to_meet_you.py
    It's a part of homework, represent  in a PyFun code :)
    
    \
    Meet Some Colleagues:
    
    First and foremost, look around your colleagues and try to make acquaintances with your fellow students. After you meet someone, note down the following information about them in a text document:
    
    · What is their name?
    
    · Where are they from?
    
    · What hobbies/pastimes do they enjoy?
    
    · Why did they pick SoftUni?

--- Nice to meet you - team info exercise/nice_to_meet_you.py
+++ Nice to meet you - team info exercise/nice_to_meet_you.py
@@ -4,17 +4,17 @@ name = input('What is your name?: ')
 place = input('Where are they from?: ')
 hobby = input('What hobbies/pastimes do they enjoy?: ')
 why = input('Why did you pick SoftUni?: ')
-print('===============================================')
+print('========================================================================')
 
-info = 'I am ' + name + ' from ' + place + '. My hobby is ' + hobby + '. I picked SoftUni because ' + why
+info = 'I am ' + name + ' from ' + place + '. My hobby is ' + hobby + '. I picked SoftUni because ' + why + '.'
 print(info)
 
 ok = False
 while not ok:
-    dir_to_save = input('Where you want to save your txt file? (a/path/you/want/to/save/to): ')
+    path_to_save = input('Where you want to save your txt file? (a/path/you/want/to/save/to): ')
     file_name = input('Enter your file name (your_name): ') + '.txt'
     try:
-        f = open(os.path.join(dir_to_save, file_name), "w")
+        f = open(os.path.join(path_to_save, file_name), "w")
         f.write(info)
         f.close()
         ok = True
