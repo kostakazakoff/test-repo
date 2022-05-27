60cffa0ac6d9e8203ac7b93ea4a7d85541845a68
dragulev <atanas.dragulev@gmail.com>
2022-05-27 05:53:34 +0300

    Update nice_to_meet_you.py

--- Nice to meet you - team info exercise/nice_to_meet_you.py
+++ Nice to meet you - team info exercise/nice_to_meet_you.py
@@ -1,22 +1,23 @@
+import os
+
 name = input('What is your name?: ')
 place = input('Where are they from?: ')
 hobby = input('What hobbies/pastimes do they enjoy?: ')
 why = input('Why did you pick SoftUni?: ')
 print('========================================================================')
-# С + е стария метод. Вече препоръчват с f стринг.
-info = f"I am {name} from {place}.\n" \
-       f"My hobby is {hobby}.\n" \
-       f"I picked SoftUni because {why}."
 
+info = 'I am ' + name + ' from ' + place + '. My hobby is ' + hobby + '. I picked SoftUni because ' + why + '.'
 print(info)
 
-file_name = f"input('Enter your file name (your_name): ').txt"
-
-# Това също се води добра практика. Няма нужда от close.
-# Отваря си файла. Върши това в индентацията и затваря.
-# Запазва файла в директорията на проекта
-with open(file_name, mode="w") as f:
-    f.write(info)
-
-print('========================================================================\n')
-print("Your file was saved in the project directory")
+ok = False
+while not ok:
+    path_to_save = input('Where you want to save your txt file? (a/path/you/want/to/save/to): ')
+    file_name = input('Enter your file name (your_name): ') + '.txt'
+    try:
+        f = open(os.path.join(path_to_save, file_name), "w")
+        f.write(info)
+        f.close()
+        ok = True
+    except:
+        print('===============================================================')
+        print('Something is wrong! Try again to enter the file name and path: ')
