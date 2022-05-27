f07d0a8a45fd3e6a58f4339478c621c663ca5755
dragulev <atanas.dragulev@gmail.com>
2022-05-27 05:39:50 +0300

    Update nice_to_meet_you.py

--- Nice to meet you - team info exercise/nice_to_meet_you.py
+++ Nice to meet you - team info exercise/nice_to_meet_you.py
@@ -6,18 +6,28 @@ hobby = input('What hobbies/pastimes do they enjoy?: ')
 why = input('Why did you pick SoftUni?: ')
 print('========================================================================')
 
-info = 'I am ' + name + ' from ' + place + '. My hobby is ' + hobby + '. I picked SoftUni because ' + why + '.'
+# С + е стария метод. Вече препоръчват с f стринг
+info = f"I am {name} from {place}.\n" \
+       f"My hobby is {hobby}.\n" \
+       f"I picked SoftUni because {why}."
+
 print(info)
 
 ok = False
 while not ok:
     path_to_save = input('Where you want to save your txt file? (a/path/you/want/to/save/to): ')
+    path_to_save = os.path.join(path_to_save)
+
     file_name = input('Enter your file name (your_name): ') + '.txt'
     try:
         f = open(os.path.join(path_to_save, file_name), "w")
         f.write(info)
         f.close()
         ok = True
-    except:
+    # Общо except като цяло не се препоръчва.
+    except FileNotFoundError:
         print('===============================================================')
-        print('Something is wrong! Try again to enter the file name and path: ')
+        print(f'The path "{path_to_save}" was not found. ')
+    except PermissionError:
+        print('===============================================================')
+        print(f'No permission to save in "{path_to_save}" or file already exists. ')
